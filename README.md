c463405f63b9cde5e17587f41737ce025ba0b316
Lo Wei Chin <weichin.lo@gmail.com>
2019-07-18 14:33:46 +0800

    update command

--- README.md
+++ README.md
@@ -13,6 +13,33 @@
 * 4.14
 * release date 2018-11-13
 
+
+### preparing for RPI 3B+
+```
+$ apt-get update
+$ apt-get install libreoffice-writer libreoffice-calc libreoffice-impress -y
+$ apt-get install cups -y
+$ apt-get install libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev \
+   pkg-config libgl1-mesa-dev libgles2-mesa-dev \
+   python-setuptools libgstreamer1.0-dev git-core \
+   gstreamer1.0-plugins-{bad,base,good,ugly} \
+   gstreamer1.0-{omx,alsa} python-dev libmtdev-dev \
+   xclip xsel libjpeg-dev -y
+
+
+$ python3 -m pip install cython==0.28.2 pillow setuptools
+$ python3 -m pip install pdf2image PyPDF3 requests
+$ python3 -m pip install https://github.com/kivy/kivy/archive/master.zip
+
+
+$ mkdir /var/app
+$ cd /var/app/
+$ git clone https://github.com/hiiragiizawa/printer-rpi.git
+
+
+```
+
+
 #### prepare env
 ##### 换源（国内必须换） 换源前也可备份下原来的文件
 
--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 apt-get update
-apt-get install libreoffice-writer -y
-apt-get install libreoffice-calc -y
-apt-get install libreoffice-impress -y
-apt-get install python3-kivy -y
+apt-get install libreoffice-writer libreoffice-calc libreoffice-impress -y
 apt-get install python3-pip -y
-apt-get install python3-sdl2 -y
-apt-get install python3-opengl -y
+apt-get install cups -y
+apt-get install libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev \
+   pkg-config libgl1-mesa-dev libgles2-mesa-dev \
+   python-setuptools libgstreamer1.0-dev git-core \
+   gstreamer1.0-plugins-{bad,base,good,ugly} \
+   gstreamer1.0-{omx,alsa} python-dev libmtdev-dev \
+   xclip xsel libjpeg-dev -y
 
-python3 -m pip install --upgrade --user pip setuptools
-python3 -m pip install --upgrade --user cython pillow
+python3 -m pip install cython==0.28.2 pillow setuptools
 python3 -m pip install pdf2image PyPDF3 requests
+python3 -m pip install https://github.com/kivy/kivy/archive/master.zip

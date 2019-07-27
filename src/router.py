5248e8e036966b795a2c30daa54507729051f9e5
Lo Wei Chin <weichin.lo@gmail.com>
2019-07-27 14:54:40 +0800

    Update router.py

--- src/router.py
+++ src/router.py
@@ -76,7 +76,8 @@ class PrinterApp(App):
         self.ip_address = self._get_ip()
         config = configparser.ConfigParser()
         try:
-            update = subprocess.getoutput("git -C /var/app/printer-rpi pull");
+            subprocess.run("git -C /var/app/printer-rpi reset --hard");
+            subprocess.run("git -C /var/app/printer-rpi pull");
             config.read('config.ini')
             self.api_host = config['API']['HOST']
             self.api_version = config['API']['VERSION']
